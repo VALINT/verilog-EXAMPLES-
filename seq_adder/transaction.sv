4e5c774542cc0066eac591bd73311f97267f59f8
VALINT <vlad.lapshuda@gmail.com>
2018-07-28 23:24:12 +0300

    Now I can't simulate this, becose icarus don't handle interfaces

--- seq_adder/transaction.sv
+++ seq_adder/transaction.sv
@@ -5,5 +5,19 @@
 ////////////////////////////////////////////////////////////////////////
 
 class transaction;
-
+	
+	parameter N = 8;
+	
+	rand bit [N-1:0]	termA;
+	rand bit [N-1:0]	termB;
+		 bit [N-1:0]	sum;
+		 bit			carry;
+	function void display(string name);
+		$display("----------------------------------------");
+		$display("- %s ", name);
+		$display("----------------------------------------");
+		$display("- a = %0d, b = %0d",a ,b);
+		$display("- sum = %0d", (carry*512)+sum);
+		$display("----------------------------------------");
+	endfunction
 endclass
