3c27208b3a2116f8ac4559ef2e0c3f0f52d47055
VALINT <vlad.lapshuda@gmail.com>
2018-09-07 14:39:53 +0300

    Simple 4-bits alu

--- alu/alu.v
+++ alu/alu.v
@@ -4,3 +4,34 @@
 //	by VAL
 ////////////////////////////////////////////////////////////////////////
 
+module alu(
+    OperandA,
+    OperandB,
+    Operation,
+    CarryIn,
+    CarryOut,
+    Answer);
+
+    parameter width = 4;
+
+    input       [width-1:0] OperandA;
+    input       [width-1:0] OperandB;
+    input       [2:0]       Operation;
+    input                   CarryIn;
+    output reg              CarryOut;
+    output reg  [width-1:0] Answer;
+
+    always @ *
+    begin
+        case(Operation)
+            3'b001  :   {CarryOut,Answer} = OperandA + OperandB + CarryIn;
+            3'b010  :   {CarryOut,Answer} = OperandA - OperandB - CarryIn;
+            3'b101  :   Answer = OperandA & OperandB;
+            3'b110  :   Answer = OperandA | OperandB;
+            3'b111  :   Answer = OperandA ^ OperandB;
+            default :   {CarryOut,Answer} = 5'h00;
+        endcase
+    end
+
+
+endmodule
--- alu/alu_tb.v
+++ alu/alu_tb.v
@@ -3,4 +3,41 @@
 //	Testbench fot ALU
 //	by VAL
 ////////////////////////////////////////////////////////////////////////
+`timescale 1ns/1ps
 
+module alu_tb ();
+
+    integer a;
+    integer b;
+    integer c;
+
+    reg     [3:0]   OperandA;
+    reg     [3:0]   OperandB;
+    reg     [2:0]   Operation;
+    reg             CarryIn;
+
+    alu DUT(
+        .OperandA(OperandA),
+        .OperandB(OperandB),
+        .Operation(Operation),
+        .CarryIn(0)
+    );
+
+    initial begin
+        for(a = 0; a < 8; a= a + 1) 
+            for(b = 0; b < 16; b= b + 1)
+                for(c = 0; c < 16; c= c + 1)
+                begin
+                    #10;
+                    OperandA = b;
+                    OperandB = c;
+                    Operation = a;
+                end
+    end
+
+    initial begin
+		$dumpfile("dump.vcd");
+		$dumpvars;
+	end
+
+endmodule
